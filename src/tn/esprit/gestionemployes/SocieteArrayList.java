9dd68807c5d7c12c5f6721e3551288e03dfd35c9
AzizMchala <aziz.mchala@example.com>
2025-11-10 14:58:48 +0100

    DÃ©but prosit 9 : gestion des employÃ©s, classe Employe, interface IGestion, et implÃ©mentation SocieteArrayList

---
+++ src/tn/esprit/gestionemployes/SocieteArrayList.java
@@ -0,0 +1,50 @@
+package tn.esprit.gestionemployes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class SocieteArrayList implements IGestion<Employe> {
+    private ArrayList<Employe> employes = new ArrayList<>();
+
+    @Override
+    public void ajouterEmploye(Employe e) {
+        employes.add(e);
+    }
+
+    @Override
+    public boolean rechercherEmploye(String nom) {
+        for (Employe e : employes) {
+            if (e.getNom().equals(nom)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean rechercherEmploye(Employe t) {
+        return employes.contains(t);
+    }
+
+    @Override
+    public void supprimerEmploye(Employe t) {
+        employes.remove(t);
+    }
+
+    @Override
+    public void displayEmploye() {
+        for (Employe e : employes) {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void trierEmployeParId() {
+        Collections.sort(employes, Comparator.comparingInt(Employe::getId));
+    }
+
+    @Override
+    public void trierEmployeParNomDépartementEtGrade() {
+        employes.sort(Comparator.comparing(Employe::getNomDepartement)
+                .thenComparing(Employe::getGrade));
+    }
+}
